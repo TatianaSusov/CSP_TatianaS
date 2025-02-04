fcacafd3fb34955f8b1cf695812e8e36a9051a3c
TatianaSusov <tatiana.susov@ucas-edu.net>
2025-02-04 09:54:00 -0700

    practiced expressions

--- Expressions/notes.py
+++ Expressions/notes.py
@@ -2,12 +2,25 @@
 #integers are numbers with no decimal points
 #floats are numbers with decimal points\
 #algorithms are a set of instructions to complete a task
-print(10+5)
-print(10-5)
-print(10*5)
-print(10/5)
-print(10**5) #exponents
-print(10//5) #Whole number division
-print(10%5) #Modulo/Modulus/MOD Gives the remainder
+#print(10+5)
+#print(10-5)
+#print(10*5)
+#print(10/5)
+#print(10**5) #exponents
+#print(10//5) #Whole number division
+#print(10%5) #Modulo/Modulus/MOD Gives the remainder
 
-print((3+7)*2)
+#print((3+7)*2)
+
+#x=2
+
+#print(x)
+#x*=3
+#print(x)
+#x/=4
+#print(x)
+#x**=2
+#print(x)
+pi = 3.14159
+print(f"{pi:.4f}")
+print(round(pi,4))
---
+++ Expressions/practice.c
@@ -0,0 +1,14 @@
+// Tatiana Susov, Expressions practice
+#include <stdio.h>
+#include <math.h>
+
+int num1 = 7-24/8*4+6;
+int num2 = 18/3-7+2*5;
+int num3
+
+
+int main(void){
+  printf("%d\n", num1);
+  printf("%d\n", num2);
+    return 0;
+}
