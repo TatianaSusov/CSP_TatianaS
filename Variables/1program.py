166b65f192f5247e273ae56ee448bdfa1c40f744
TatianaSusov <tatiana.susov@ucas-edu.net>
2025-02-04 08:46:51 -0700

    finsished

--- Variables/1program.py
+++ Variables/1program.py
@@ -3,4 +3,4 @@ Greeting="welcome to my program"
 print(Greeting)
 note="it can't do much of  anything yet"
 response = input ("What is your name?")
-print(response+ ",cool. sorry this program isn't very interesting, i'm only a beginner programmer.")
+print(response+ ",Thats a cool name. sorry this program isn't very interesting, i'm only a beginner programmer.")
