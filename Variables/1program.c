70cfecda00c2276aa7b13b536e5eb965cdd9bddb
TatianaSusov <tatiana.susov@ucas-edu.net>
2025-02-03 22:49:08 -0700

    finished program c

---
+++ Variables/1program.c
@@ -0,0 +1,12 @@
+// Tatiana Susov C first program
+#include <stdio.h>
+
+
+int main()
+{
+    char name[421345];
+  printf("what's your name?");
+  scanf("%s", name);
+  printf("hello", name);
+    return 0;
+}
