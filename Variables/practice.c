71c1cac1e5b4db909f3eeb31bcc66c1c71e597ef
TatianaSusov <tatiana.susov@ucas-edu.net>
2025-02-03 19:51:32 -0700

    c practice

--- Variables/practice.c
+++ Variables/practice.c
@@ -1,8 +1,19 @@
 //Tatiana Susov, Variables Practice
+//your name
+//a number between 1 and 10
+//a number between 100 and 1000
+//what you had for breakfast 
+//your favorite color
+//the school name
+//the year
+//your eye color
+//your age
+//your favorite subject in school 
 #include <stdio.h>
 
 
 int main(void){
   printf("Hello world");
+  printf("Tatiana");
     return 0;
 }
