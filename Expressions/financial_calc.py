bc970629da744a6c215876adabd406389fe0de11
TatianaSusov <tatiana.susov@ucas-edu.net>
2025-02-06 09:48:39 -0700

    started calculator

--- Expressions/financial_calc.py
+++ Expressions/financial_calc.py
@@ -1,1 +1,41 @@
 # Tatiana Susov, Financial Calculator Python
+
+# write a print statement telling the user what the program is (budget calculator)
+greeting="Welcome to your"
+# Ask for monthly income (user input)
+income = float(input("What is your monthly income\n"))
+# Ask for rent (user input)
+income = float(input("What is your monthly rent\n"))
+# Ask for utilities amount (user input)
+income = float(input("What is your monthly spending on utilities\n"))
+# Ask for groceries amount (user input)
+
+# Ask for transportation amount (user input)
+
+# Calculate savings as 10% of income (variable)
+
+# Calculate spending money income - (rent+utilities+groceries+transportation+savings) (variable)
+
+# Calculate percent of rent (rent/income) (variable)
+
+# Calculate percent of utilities (utilities/income) (variable)
+
+# Calculate percent of groceries (groceries/income) (variable)
+
+#Calculate percent of transportation (transportation/income) (variable)
+
+# Calculate percent of spending money (spending money/income) (variable)
+
+#Calculate percent of savings (savings/income) (variable)
+
+# tell user category spending amount and percent for rent ("You spend $xx.xx on rent and that is xx% of your income")
+
+# tell user category spending amount and percent for utilities ("You spend $xx.xx on utilities and that is xx% of your income")
+
+# tell user category spending amount and percent for groceries ("You spend $xx.xx on groceries and that is xx% of your income")
+
+# tell user category spending amount and percent for transportation ("You spend $xx.xx on transportation and that is xx% of your income")
+
+# tell user category spending amount and percent for spending ("You spend $xx.xx on spending and that is xx% of your income")
+
+# tell user category spending amount and percent for saving ("You spend $xx.xx on savings and that is xx% of your income")
