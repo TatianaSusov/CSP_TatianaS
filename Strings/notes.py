6f007c769d24083752428f0a7ed656d2cdc4f010
TatianaSusov <tatiana.susov@ucas-edu.net>
2025-02-10 09:53:10 -0700

    notes

---
+++ Strings/notes.py
@@ -0,0 +1,16 @@
+# Tatiana Susov, Strings Notes Python
+
+# Words in porgramming (data type) "" ''
+
+#name = input ("What is your first name?\n").strip().upper()
+#name = input ("please give me a number\n").strip().upper()
+#print(name+ "2")
+#print(f" Hello {name} welcome to my program")
+
+#num = 5*2+name
+#print(num)
+#print(name + "2")
+sentence = "The quick brown fox jumps over the lazy dog."
+
+print(sentence.find("fox"))
+print(sentence.)
