fe2d19dfc7cebc32210f0e973bb4d540ba412127
TatianaSusov <tatiana.susov@ucas-edu.net>
2025-02-11 09:53:06 -0700

    started practices

---
+++ Strings/practice.c
@@ -0,0 +1,9 @@
+//Tatiana Susov, Name Decorator, C
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void){
+  printf("%s\n",);
+    return 0;
+}
---
+++ Strings/practice.py
@@ -0,0 +1,1 @@
+# Tatiana Susov, Practice, Name Decorator, Py
