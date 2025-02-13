f2ca3a925f4e1358afde8cc4e9e398dc83845373
TatianaSusov <tatiana.susov@ucas-edu.net>
2025-02-13 09:52:53 -0700

    worked on silly strings

--- Strings/silly_sentences.c
+++ Strings/silly_sentences.c
@@ -1,11 +1,31 @@
 //Tatiana Susov, Silly Sentences, C
 #include <stdio.h>
 //String variables for my user inputs (minimum of 3)
-//introduce user to program... tell them what is happening (print statement)
-// create user inputs (print statements with questions AND scanf to collect the info)(same as the number of variables)
-// insert variables into the sentence to show the user (print statement, only 1)
-
+ char animal[943287];
+ char verb[57984203];
+ char place[5793454];
+ char feeling[57439857];
+ char noun[9852340];
 int main(void){
-  printf("Hello world");
+    //introduce user to program... tell them what is happening (print statement)
+    printf("Hello user and welcome to the crazy fantastic amazing bizarre mad lib program, lets make a mad lib together!");
+// create user inputs (print statements with questions AND scanf to collect the info)(same as the number of variables)
+ printf("\nPlease input a random animal.");
+ scanf("%s", animal );
+ printf("\nNow input a past tense verb");
+ scanf("%s", verb );
+ printf("\nNow, input a place of your choice");
+ scanf("%s", place );
+ printf("\nNow input a feeling.");
+ scanf("%s", feeling);
+ printf("\nLast but not least, please input a noun.");
+ scanf("%s", noun );
+ printf("\nHere is your mad lib!");
+ printf("%s\n The ");
+ printf("%s", animal );
+ printf("%s", verb );
+ printf("all the way to ", place );
+// insert variables into the sentence to show the user (print statement, only 1) example: "hello %s", name
+
     return 0;
 }
