1ba32505d9d5797ced3076128d8fc3f311a3d2e2
TatianaSusov <tatiana.susov@ucas-edu.net>
2025-02-13 06:45:39 +0000

    failed to understand why C is so complicated and will end up turning in late because I didn't get enough time to work on it in class and apparently C doesn't like me when I am at home.

--- Strings/practice.c
+++ Strings/practice.c
@@ -4,17 +4,17 @@
 
 int main(void){
  
-  //char phrase [] = "here is your name decorated!";
+  char phrase [] = "here is your name decorated!";
   char name [73453];
   char d_1 [] = "---";
   char d_2 [] = "---";
   printf("Welcome to the name decorator! What is your first name?");
   scanf("%s", name);
-  //printf("%s", phrase);
+  printf("%s", phrase);
   strcat(d_1, name);
   strcat(name, d_2);
-  printf("%s", d_1);
-  printf("%s",d_2);
+  printf("%s", name);
+  printf("%s\n", d_1); printf("%s\n",d_2);
   
   
 
