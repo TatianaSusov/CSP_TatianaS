21b53fc4eb68d1312c39607731c60c058a0d969b
TatianaSusov <tatiana.susov@ucas-edu.net>
2025-02-14 09:52:58 -0700

    python silly sentences work

--- Strings/notes.py
+++ Strings/notes.py
@@ -2,7 +2,7 @@
 
 # Words in porgramming (data type) "" ''
 
-#name = input ("What is your first name?\n").strip().upper()
+name = input ("What is your first name?\n").strip().upper()
 #name = input ("please give me a number\n").strip().upper()
 #print(name+ "2")
 #print(f" Hello {name} welcome to my program")
@@ -10,7 +10,7 @@
 #num = 5*2+name
 #print(num)
 #print(name + "2")
-sentence = "The quick brown fox jumps over the lazy dog."
+#sentence = "The quick brown fox jumps over the lazy dog."
 
 #print(sentence.find("fox"))
 #print(sentence.)
@@ -19,5 +19,5 @@ sentence = "The quick brown fox jumps over the lazy dog."
 #print(len(sentence))
 #name = "tatiana"
 #print("welcome to the program "+name)
-percent = 89
-print(f"your grade is {percent:.1f}")
+#percent = 89
+#print(f"your grade is {percent:.1f}")
