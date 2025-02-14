ae3f90f0924dab04052447262d60c23460c7bf4c
TatianaSusov <tatiana.susov@ucas-edu.net>
2025-02-14 01:33:07 +0000

    finished c silly sentences

--- Strings/silly_sentences.c
+++ Strings/silly_sentences.c
@@ -1,31 +1,37 @@
 //Tatiana Susov, Silly Sentences, C
 #include <stdio.h>
 //String variables for my user inputs (minimum of 3)
- char animal[943287];
- char verb[57984203];
- char place[5793454];
- char feeling[57439857];
- char noun[9852340];
+ char animal [943287];
+ char verb [57984203];
+ char place [5793454];
+ char feeling [57439857];
+ char noun [9852340];
+ 
 int main(void){
-    //introduce user to program... tell them what is happening (print statement)
-    printf("Hello user and welcome to the crazy fantastic amazing bizarre mad lib program, lets make a mad lib together!");
-// create user inputs (print statements with questions AND scanf to collect the info)(same as the number of variables)
- printf("\nPlease input a random animal.");
+   
+ printf("Hello user and welcome to the crazy fantastic amazing bizarre mad lib program, lets make a mad lib together!");
+ printf("\nPlease input a random animal (single word).");
  scanf("%s", animal );
- printf("\nNow input a past tense verb");
+ printf("\nNow input a past tense verb (single word) ");
  scanf("%s", verb );
- printf("\nNow, input a place of your choice");
+ printf("\nNow, input a place of your choice (single word) ");
  scanf("%s", place );
- printf("\nNow input a feeling.");
+ printf("\nNow input a feeling (single word). ");
  scanf("%s", feeling);
- printf("\nLast but not least, please input a noun.");
+ printf("\nLast but not least, please input a noun (single word). ");
  scanf("%s", noun );
- printf("\nHere is your mad lib!");
- printf("%s\n The ");
+ printf("\nHere is your mad lib! "); 
+ printf("\n The ");
  printf("%s", animal );
- printf("%s", verb );
- printf("all the way to ", place );
-// insert variables into the sentence to show the user (print statement, only 1) example: "hello %s", name
+ printf(" ");
+ printf("%s",verb );
+ printf(" all the way to ");
+ printf("%s", place);
+ printf(" with a/an ");
+ printf("%s", feeling);
+ printf(" look on his face. Don't worry, he didn't forget his ");
+ printf("%s", noun);
+ printf(".");
 
     return 0;
 }
