06378f8470440a4b982941f531ab9ba4ffe88012
TatianaSusov <tatiana.susov@ucas-edu.net>
2025-02-14 20:30:31 +0000

    finished .py silly sentences

--- Strings/silly_sentences.py
+++ Strings/silly_sentences.py
@@ -1,12 +1,10 @@
 # Tatiana Susov, Silly Sentences Python
-#name = input ("What is your first name?\n")
-
 
 print ("Welcome to the silly sentences program, a program designed to make a silly mad-lib. Let's begin!\n")
 animal = input ("Please input the name of an animal\n").strip()
 noun_1 = input ("Please input a noun of your choice\n").strip()
 verb_pt = input ("Please input a present tense verb\n").strip()
 noun_2 = input ("Please input a second noun of your choice\n").strip()
-place = input ("Please input a place of your choice").strip()
+place = input ("Please input a place of your choice\n").strip()
 print ("Here is your mad-lib!")
-print ("My", animal,"always takes my", noun_1, "when I am", verb_pt+". So now I have to chase him around with a/an", noun_2, "in my hand so I can get my", noun_1, "back before going to", place+ ".") 
+print ("My", animal,"always takes my", noun_1, "when I am", verb_pt+". Now I have to chase him around with a/an", noun_2, "in my hand so I can get my", noun_1, "back before going to", place+ ".") 
