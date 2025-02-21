93333d30d4a772a3564f7c16699cfa60a7689f27
TatianaSusov <tatiana.susov@ucas-edu.net>
2025-02-21 09:45:14 -0700

    fin cal py done

---
+++ Functions/financial_calc_update.c
@@ -0,0 +1,45 @@
+//Tatiana Susov, Update Financial Calculator C
+#include <stdio.h>
+
+int price(){
+
+}
+
+int main(void){
+
+ printf("%s\n", "welcome to your personal financial/budget calculater, lets begin!");
+ float income, rent, utilities, groceries, transportation, savings, spending,total;
+ float pincome, prent, putilities, pgroceries, ptransportation, psavings, pspending;
+ printf("What is your monthly income?:");
+ scanf("%f", &income);
+ printf("What is your monthly rent?:");
+ scanf("%f", &rent);
+ printf("What is your monthly spending on utilities?:");
+ scanf("%f", &utilities);
+ printf("What is your monthly spending on transportation?:");
+ scanf("%f", &transportation);
+ printf("How much do you try to save monthly?:");
+ scanf("%f", &savings);
+ spending = rent+utilities+groceries+transportation;
+ savings = income * .2;
+ printf("Your monthly income is $%.2f\n",income);
+ total = income-savings-spending;
+ printf("Your monthly savings is $%.2f\n",savings);
+ printf("Your monthly total is $%.2f\n",total);
+ printf("Your monthly spending is $%.2f\n",spending);
+ prent=rent/income*100;
+ putilities=utilities/income*100;
+ pgroceries=groceries/income*100;
+ ptransportation=transportation/income*100;
+ psavings=savings/income*100;
+ pspending=spending/income*100;
+ printf("Your expenses are %.1f%% of your income\n",pspending);
+ printf("Your rent is %.1f%% of your income\n",prent);
+ printf("Your utilities payment is %.1f%% of your income\n",putilities);
+ printf("Your grocery spending is %.1f%% of your income\n",pgroceries);
+ printf("Your transportation fees is %.1f%% of your income\n",ptransportation);
+ printf("Your savings is %.1f%% of your income\n",psavings);
+  
+
+    return 0;
+}
--- Functions/financial_calc_update.py
+++ Functions/financial_calc_update.py
@@ -2,20 +2,25 @@
 
 def info(income, amount, type):
     pertype = amount/income*100
-    print(f"You spend ${amount:.2f} on {type} and that is {pertype:.0f}% of your income")
-
+    print(f"You spend ${amount:.2f} on {type} and that is {pertype:.0f}% of your income.")
+   
 
 print("Welcome to your personal financial/budget calculator, lets begin!")
-income = float(input("What is your monthly income?\n"))
-rent = float(input("What is your monthly rent?\n"))
-utilities = float(input("What is your monthly spending on utilities?\n"))
-groceries = float(input("What is your monthly spending on groceries?\n"))
-transportation = float(input("What is your monthly spending on transportation?\n"))
+
+def price(price):
+    return float(input(f"what is your monthly spending on {price}? "))
+
+income = price("income")
+rent = price("rent")
+utilities = price("utilities")
+groceries = price("groceries")
+transportation = price("transportation")
 savings = income/10
-total_spending = rent-utilities-groceries-transportation-savings
+total_spending = income-(rent+utilities+groceries+transportation+savings)
 info(income, rent, "rent")
 info(income, utilities, "utilities")
 info(income, groceries, "groceries")
 info(income, transportation, "transportation")
 info(income, savings, "savings")
+info(income, total_spending, "everything in total")
 
