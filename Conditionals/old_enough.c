94db8ccc42e05ba2a657fcf2f209786f47d58752
TatianaSusov <tatiana.susov@ucas-edu.net>
2025-02-25 09:53:10 -0700

    started practices

---
+++ Conditionals/old_enough.c
@@ -0,0 +1,9 @@
+//Tatiana Susov, Old Enough C Practice
+#include <stdio.h>
+
+
+int main(void){
+  
+  
+    return 0;
+}
---
+++ Conditionals/old_enough.py
@@ -0,0 +1,11 @@
+# Tatiana Susov Old Enough Py
+
+print("Hello, welcome to my program. This program will tell you if you are old enough to have certain oppotunities. ")
+
+name = input("What is your name?\n")
+
+if name == "Vienna":
+    print("Oh your the teacher.... never mind.")
+    print("You're old enough for everything anyways.")
+else:
+    print("You are not Ms Larose, you must be a student at UCAS or someone else entirely different.")
