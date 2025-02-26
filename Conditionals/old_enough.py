a4e8964334ade1249cfaf64de3092147cf775e62
TatianaSusov <tatiana.susov@ucas-edu.net>
2025-02-26 04:32:36 +0000

    tried and likely failed on python Old enough assignment, don't worry, I'm not finished with it yet anyways.

--- Conditionals/old_enough.py
+++ Conditionals/old_enough.py
@@ -1,11 +1,18 @@
 # Tatiana Susov Old Enough Py
 
-print("Hello, welcome to my program. This program will tell you if you are old enough to have certain oppotunities. ")
+print("Hello, welcome to my program.\n This program will tell you if you are old enough to have certain oppotunities. ")
 
 name = input("What is your name?\n")
 
 if name == "Vienna":
-    print("Oh your the teacher.... never mind.")
-    print("You're old enough for everything anyways.")
+    print("Oh your the teacher.... never mind.\n")
+    print("You're old enough for everything anyways.\n")
+    print("You can still do the program though.\n")
 else:
-    print("You are not Ms Larose, you must be a student at UCAS or someone else entirely different.")
+    print("Hi friend.")
+
+num = input("Please enter your current age.\n")
+if num >= 18:
+    print(f"Wow, {num}! this means that you can vote on government related things! ")
+else:
+    print(f"You are not old enough to vote on governemnt related things.")
