38b1a781404e3d25d81300ebc08e76cf263d9b5c
TatianaSusov <tatiana.susov@ucas-edu.net>
2025-03-07 09:43:24 -0700

    Finished All fizzbuzz assignments

--- Loops/notes.c
+++ Loops/notes.c
@@ -55,12 +55,12 @@ while(iterator >=0){
     //Sets what I count by
     iterator -= 10;
 }
-char movies[][20] ={"Cinderella", "the Smurf Movie", "Transformers", "Cars", "Up", "1984"};
-int mlength = sizeof(movies)/sizeof(movies[0]);
-int m = 0;
-while(m<mlength){
-    printf("%s\n", movies[m]);
-    m++;
-}
+//char movies[][20] ={"Cinderella", "the Smurf Movie", "Transformers", "Cars", "Up", "1984"};
+//int mlength = sizeof(movies)/sizeof(movies[0]);
+//int m = 0;
+//while(m<mlength){
+  //  printf("%s\n", movies[m]);
+ //   m++;
+//}
     return 0;
 }
